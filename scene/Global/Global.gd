2092c529dbd61b9261eb4e8cea3b42cb68092e15
pibolib <pibolib@gmail.com>
2023-01-14 17:23:25 -0500

    initial commit, creates project and base elements

---
+++ scene/Global/Global.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var player_stats_default: Dictionary = {
+	"HP": 3,
+	"Score": 0,
+}
+
+var player_stats: Dictionary = player_stats_default.duplicate(true)
---
+++ scene/MainGame/Player/Player.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+signal player_hit
+
+const FRICTION_RATE: float = 20
+const PLAYER_SPEED: float = 75
+
+var speed_multiplier = 1
+var velocity: Vector2 = Vector2(0,0)
+var stats: Dictionary
+var invulnerable: bool = false
+
+func _ready():
+	#self.connect("player_hit",Global._on_player_hit)
+	self.connect("player_hit",take_damage)
+	#stats = Global.player_stats
+
+func _process(delta):
+	if Input.is_action_pressed("ingame_move_down"):
+		velocity.y = PLAYER_SPEED * speed_multiplier
+	elif Input.is_action_pressed("ingame_move_up"):
+		velocity.y = -PLAYER_SPEED * speed_multiplier
+	if Input.is_action_pressed("ingame_move_right"):
+		velocity.x = PLAYER_SPEED * speed_multiplier
+	elif Input.is_action_pressed("ingame_move_left"): 
+		velocity.x = -PLAYER_SPEED * speed_multiplier
+	speed_multiplier = 1 - 0.5*float(Input.is_action_pressed("ingame_focus"))
+	position += velocity * delta
+	velocity = lerp(velocity,Vector2(0,0),FRICTION_RATE*delta)
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.name == "EnemyHurtbox" and !invulnerable: 
+		emit_signal("player_hit")
+
+func take_damage() -> void:
+	$Invulnerability.start(2)
+	$AnimationPlayer.play("Invulnerability")
+	invulnerable = true
+
+func _on_invulnerability_timeout() -> void:
+	invulnerable = false
+
