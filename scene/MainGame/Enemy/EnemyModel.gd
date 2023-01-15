d5a86bf4684312a237ae5ccf04c06da334a1aabb
pibolib <pibolib@gmail.com>
2023-01-15 18:14:42 -0500

    revamped enemy sprite, added animation, added pattern + model systems

---
+++ scene/MainGame/Enemy/EnemyModel.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var aim_dir: float = 0
+var current_anim = ""
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_anim("Idle")
+	$AnimationPlayer.advance(global_position.x/300)
+
+func _process(delta):
+	dynamic_anim(current_anim, $AnimationPlayer.current_animation_position)
+
+func set_anim(anim_name: String) -> void:
+	$AnimationPlayer.play(anim_name)
+	current_anim = anim_name
+
+func set_aim_dir(dir: float) -> void:
+	aim_dir = dir
+
+func dynamic_anim(anim: String, time: float) -> void:
+	match anim:
+		"Attack":
+			$Torso/GunAim.rotation = aim_dir - PI/2
+			$Torso/Arm1.position = $Torso/GunAim.position - Vector2(cos(aim_dir-PI/16),sin(aim_dir-PI/16))*3 - Vector2(0,1)
+			$Torso/Arm2.position = $Torso/GunAim.position + Vector2(cos(aim_dir+PI/16),sin(aim_dir+PI/16))*3 - Vector2(0,1)
+		_:
+			pass
--- scene/MainGame/EnemyPattern/EnemyPattern.gd
+++ scene/MainGame/EnemyPattern/EnemyPattern.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 class_name EnemyPattern
 var bullet_basic: PackedScene = preload("res://scene/MainGame/EnemyBullet/EnemyBullet.tscn")
+@onready var game_level: Node = get_parent().get_parent() # gets game level 
 
 func fire_bullet_basic(angle_offset: float, speed: float, target_player: bool = true) -> void:
 	var new_bullet = bullet_basic.instantiate()
-	
+	if target_player:
+		new_bullet.angle = get_angle_to_player(get_parent().position) + deg_to_rad(angle_offset)
+	else:
+		new_bullet.angle = deg_to_rad(angle_offset)
+	new_bullet.speed = speed
+	new_bullet.position = get_parent().position
+	game_level.add_child(new_bullet)
 
 func get_angle_to_player(start_pos: Vector2) -> float:
 	var target = Global.player_pos
