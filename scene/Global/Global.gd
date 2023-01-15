a67f177b4faab343b73cb74754ce60cd2d43dd32
pibolib <pibolib@gmail.com>
2023-01-15 15:30:50 -0500

    potential fix for mouse issues

--- scene/Global/Global.gd
+++ scene/Global/Global.gd
@@ -17,7 +17,7 @@ var mouse_sensitivity: float = 150
 var maximized: bool = false
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
 	if Input.is_action_just_pressed("ingame_maximize"):
@@ -26,18 +26,27 @@ func _process(delta):
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		maximized = !maximized
-	var viewport_position: Vector2 = get_viewport().get_visible_rect().size/2
-	var mouse_position: Vector2 = get_viewport().get_mouse_position()
-	var mouse_delta: float = mouse_position.x - viewport_position.x 
-	print(mouse_delta/mouse_sensitivity)
-	get_viewport().warp_mouse(get_viewport().get_visible_rect().size/2) 
-	angle -= mouse_delta / mouse_sensitivity
-	angle = clamp(angle, -PI/2, PI/2)
 
-func _on_player_hit():
+func _input(event):
+	if event is InputEventMouseMotion:
+		angle -= event.relative.x / mouse_sensitivity
+		angle = clamp(angle,-PI/2,PI/2)
+
+func _on_player_hit() -> void:
 	player_stats.HP -= 1
 	emit_signal("player_hp_changed",player_stats.HP)
 
-func _on_player_fire():
+func _on_player_fire() -> void:
 	player_stats.Bullets -= 1
 	emit_signal("player_bullets_changed",player_stats.Bullets)
+	
+func _on_player_reload() -> void:
+	player_stats.Bullets += 1
+	emit_signal("player_bullets_changed",player_stats.Bullets)
+
+func _on_score_change(value: int) -> void:
+	player_stats.Score += value
+	emit_signal("score_changed",player_stats.Score)
+
+func _on_player_graze() -> void:
+	_on_score_change(10)
