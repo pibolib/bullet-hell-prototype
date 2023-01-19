e68e57aa3959f2e0aca95cd760e75471982dd3ba
pibolib <pibolib@gmail.com>
2023-01-18 21:42:01 -0500

    implemented dodge system (resolves #30), half implemented sniper enemy)

--- scene/MainGame/PlayerBullet/Bullet.gd
+++ scene/MainGame/PlayerBullet/Bullet.gd
@@ -1,9 +1,11 @@
 extends Node2D
+class_name PlayerBullet
 
 var hit_explosion: PackedScene = preload("res://scene/FX/PlayerHitExplosion.tscn")
 @export var start_point: Vector2 = Vector2(0,0)
 @export var target_point: Vector2 = Vector2(0,0)
 var angle: float = 0
+var collision_point = Vector2(0,0)
 
 func _ready():
 	target_point = start_point + Vector2(sin(angle),cos(angle))*1000
@@ -18,17 +20,16 @@ func _ready():
 	var collider = $RayCast2D.get_collider()
 	if collider != null:
 		if collider.state != 0:
-			collider.take_damage()
-			var collision_point = $RayCast2D.get_collision_point()
-			$Visual.points = [start_point,collision_point]
-			$EnemyBulletVoid.position = lerp(start_point,collision_point,1)
-			$EnemyBulletVoid.scale.x = start_point.distance_to(collision_point)
-			var new_effect = hit_explosion.instantiate()
-			new_effect.position = $RayCast2D.get_collision_point()
-			get_parent().add_child(new_effect)
+			collision_point = $RayCast2D.get_collision_point()
+			if(collider._on_bullet_collide(self)):
+				$Visual.points = [start_point,collision_point]
+				$EnemyBulletVoid.position = lerp(start_point,collision_point,1)
+				$EnemyBulletVoid.scale.x = start_point.distance_to(collision_point)
+				var new_effect = hit_explosion.instantiate()
+				new_effect.position = $RayCast2D.get_collision_point()
+				get_parent().add_child(new_effect)
 	else:
 		$EnemyBulletVoid.scale.x = start_point.distance_to(target_point)
-
 
 func _on_timer_timeout():
 	$Timer.stop()
