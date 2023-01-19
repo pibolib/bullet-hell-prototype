759a1717bf100dce071b5fc7194eb63d0d5076b4
pibolib <pibolib@gmail.com>
2023-01-18 20:57:00 -0500

    added attack drone enemy, waiting on model (resolves #24)

--- scene/MainGame/EnemyPattern/EnemyPatternCircleShot.gd
+++ scene/MainGame/EnemyPattern/EnemyPatternCircleShot.gd
@@ -2,6 +2,6 @@ extends EnemyPattern
 
 
 func _ready():
-	for i in 10: #fires 10 rounds in a circular pattern
-		fire_bullet_basic(i*36,70)
+	for i in 16: #fires 16 rounds in a circular pattern
+		fire_bullet_basic(i*22.5,70)
 	queue_free()
